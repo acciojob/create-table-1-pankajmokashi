4f613ee9e02a6dc8104b483452ae3375d259e653
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-05-18 17:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	  const table = document.getElementById("sampleTable")
+	const tr = 
+		`
+			<tr><td>New cell1</td> 
+			<td>New cell2</td></tr> 
+	   `
+  table.innerHTML = tr + table.innerHTML
 }
